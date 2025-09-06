2b0b09acc8fa1e775772326c10752d627fc813df
Abhishek Kumar <btech25002.24@bitmesra.ac.in>
2025-09-06 17:27:58 +0000

    check emtire code and fix this error

--- next.config.ts
+++ next.config.ts
@@ -26,13 +26,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    // Exclude firebase-admin from client-side bundles
-    if (!isServer) {
-        config.externals.push('firebase-admin');
-    }
-    return config;
-  }
 };
 
 export default nextConfig;
