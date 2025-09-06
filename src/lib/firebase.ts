7cb2ff515009694eae4a5f5c8fd705a3238c446e
Abhishek Kumar <btech25002.24@bitmesra.ac.in>
2025-09-06 14:50:50 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "truth-lens-1",
-  "appId": "1:755035155214:web:23ed09256e942f9d84055f",
-  "storageBucket": "truth-lens-1.firebasestorage.app",
-  "apiKey": "AIzaSyB3Xn2M1y-tlN2KQI_WUbPd89GlJfcA-9s",
-  "authDomain": "truth-lens-1.firebaseapp.com",
-  "messagingSenderId": "755035155214"
+  apiKey: "AIzaSyB3Xn2M1y-tlN2KQI_WUbPd89GlJfcA-9s",
+  authDomain: "truth-lens-1.firebaseapp.com",
+  projectId: "truth-lens-1",
+  storageBucket: "truth-lens-1.firebasestorage.app",
+  messagingSenderId: "755035155214",
+  appId: "1:755035155214:web:23ed09256e942f9d84055f"
 };
 
 // Initialize Firebase
